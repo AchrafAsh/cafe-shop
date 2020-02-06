f33aec567265de1f94dfe514bbfe132cf952e028
Ait sidi hammou Achraf <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-02-06 20:44:05 +0100

    Add snipcart

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,18 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from '../components/globals/BackgroundSection'
 import Info from '../components/home/Info'
+import Menu from '../components/home/Menu'
+import Products from '../components/home/Products'
+import Contact from '../components/home/Contact'
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <BackgroundSection img={data.img.childImageSharp.fluid} title="Cafe Shop" styleClass="default-background"/>
     <Info />
+    <Menu items={data.menu}/>
+    <Products />
+    <Contact />
   </Layout>
 )
 
@@ -20,6 +26,24 @@ export const query = graphql`
       childImageSharp{
         fluid{
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu:allContentfulCoffeeItem{
+        edges{
+          node{
+            id
+            title
+            descritption{
+              descritption
+            }
+            price
+            category
+            image{
+              fixed(width:50,height:50){
+                ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
